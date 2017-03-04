0b4ff0b85395f4bb1f14495ee95ccf553bdbbf01
Moses Alade <malade1@hawk.iit.edu>
2017-03-04 01:20:38 -0600

    adjust js codes

--- js/site.js
+++ js/site.js
@@ -6,21 +6,21 @@ jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
     
-  $('#email).on('keyup focus on blur', function(){
+  $('#email').on('keyup focus blur', function(){
     var currentValue = $(this).val();
-    var validPattern = $(this).attr('pattern');
-    if(currentValue.match(validPattern)){
+    var vaildPattern = $(this).attr('pattern');
+    if(currentValue.match(vaildPattern)){
       $('#submit').addClass('active');
-    } 
-      else{
-        $('#submit').removeClass('active');
-    }    
+    }
+    else{
+      $('#submit').removeClass('active');
+    }
   });
   function clearForm() {
   $('#form')[0].reset();
 }
   $('#form').submit(function(event) {
-    alert('Thank you! We will get back to you ASAP!');
+    alert(Thank you! We will get back to you ASAP!');
     event.preventDefault();
 
     clearForm();

e17d78259d166f46decd4bc68f99dfc6232db249
Moses Alade <malade1@hawk.iit.edu>
2017-03-03 23:35:35 -0600

    adjusted line space in js folder

--- js/site.js
+++ js/site.js
@@ -2,22 +2,25 @@ $.noConflict();
 jQuery(function($) {
  $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-  $('#email).on('keyup focus on blur', function(){
-    var currentValue = $(this).val();
+  
+  $('#email).on('keyup focus on blur', function() {
+    var currentValuename = $(this).val();
     var validPattern =$(this).attr('pattern');
-      console.log(currentValue, validPattern);
     if(currentValue.match(validPattern)){
   $('#submit').addClass('active');
-    } else{
+    } 
+    else{
   $('#submit').removeClass('active');
     }
    });
   function clearForm() {
   $('#form')[0].reset();
-    }
+ }
+ 
   $('#form').submit(function(event) {
   alert('Thank you! We will get back to you ASAP!');
   event.preventDefault();
+  
   clearForm();
   });
 });

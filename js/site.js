92968ab2dbaaedb1003cb253ba153d66e275ad02
Moses Alade <malade1@hawk.iit.edu>
2017-03-03 23:21:35 -0600

    Adjusted the line spancing

--- js/site.js
+++ js/site.js
@@ -1,29 +1,23 @@
 $.noConflict();
-
 jQuery(function($) {
-  $('html').removeClass('nojs');
+ $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-    
   $('#email).on('keyup focus on blur', function(){
     var currentValue = $(this).val();
     var validPattern =$(this).attr('pattern');
-    
-    console.log(currentValue, validPattern);
+      console.log(currentValue, validPattern);
     if(currentValue.match(validPattern)){
-      $('#submit').addClass('active');
+  $('#submit').addClass('active');
     } else{
-      $('#submit').removeClass('active');
+  $('#submit').removeClass('active');
     }
-    
-  });
-
+   });
   function clearForm() {
   $('#form')[0].reset();
-}
+    }
   $('#form').submit(function(event) {
-    alert('Thank you! We will get back to you ASAP!');
-    event.preventDefault();
-
-    clearForm();
+  alert('Thank you! We will get back to you ASAP!');
+  event.preventDefault();
+  clearForm();
   });
 });

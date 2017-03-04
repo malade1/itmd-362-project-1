52f862486d1fd22daa44ed9ea55fdf5c41556264
Moses Alade <malade1@hawk.iit.edu>
2017-03-04 01:35:56 -0600

    adjusted js codes

--- js/site.js
+++ js/site.js
@@ -2,24 +2,27 @@
 $.noConflict();
 
 //jQuery 3.x-style ready event and locally scoped $
-jQuery(function($){
+jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
     
-  $('#email').on('keyup focus blur', function(){
+  $('#email).on('keyup focus on blur', function(){
     var currentValue = $(this).val();
-    var vaildPattern = $(this).attr('pattern');
-    if(currentValue.match(vaildPattern)){
+    var validPattern = $(this).attr('pattern');
+    
+    console.log(currentValue, validPattern);
+    if(currentValue.match(validPattern)){
       $('#submit').addClass('active');
-    }
-    else{
+    } else{
       $('#submit').removeClass('active');
     }
+    
   });
+
   function clearForm() {
   $('#form')[0].reset();
 }
-  $('#form').submit(function(event) {
+  $('#form').submit(function(event){
     alert('Thank you! We will get back to you ASAP!');
     event.preventDefault();
 

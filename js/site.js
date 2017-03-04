ad803e37f2f6ed3335fe11a617e7b3203c8c38c2
Moses Alade <malade1@hawk.iit.edu>
2017-03-03 21:35:29 -0600

    Added padding to my css

--- js/site.js
+++ js/site.js
@@ -1,5 +1,29 @@
 $.noConflict();
-(function($) {
-  $(document).ready(function() {
-    $('#form').on('submit', function(event) {
-      event.preventDefault();
+
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+    
+  $('#email).on('keyup focus on blur', function(){
+    var currentValue = $(this).val();
+    var validPattern =$(this).attr('pattern');
+    
+    console.log(currentValue, validPattern);
+    if(currentValue.match(validPattern)){
+      $('#submit').addClass('active');
+    } else{
+      $('#submit').removeClass('active');
+    }
+    
+  });
+
+  function clearForm() {
+  $('#form')[0].reset();
+}
+  $('#form').submit(function(event) {
+    alert('Thank you! We will get back to you ASAP!');
+    event.preventDefault();
+
+    clearForm();
+  });
+});

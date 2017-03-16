a513c00f6085ec31f90f0267345ff6aadc5e8f47
Moses Alade <malade1@hawk.iit.edu>
2017-03-16 11:03:07 -0500

    removed empty spcaces in js folder

--- js/site.js
+++ js/site.js
@@ -1,11 +1,9 @@
 //Tell jQuery to give up the dollar sign 
 $.noConflict();
-
 //jQuery 3.x-style ready event and locally scoped $
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-
   $('#email').on('keyup focus blur', function(){
     var currentValue = $(this).val();
     var vaildPattern = $(this).attr('pattern');
@@ -22,7 +20,6 @@ jQuery(function($){
   $('#form').submit(function(event) {
     alert('Thank for signing up!');
     event.preventDefault();
-
     clearForm();
   });
 });
